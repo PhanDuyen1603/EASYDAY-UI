56d47fd2f83eeeb752eb3b2c29919c2bb97b349c
DuyenPhan <20120567@student.hcmus.edu.vn>
2021-12-06 23:20:31 +0700

    update new prod homepage

---
+++ components/home/Section3.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <div class="d-flex">
+      <div
+        class="elementor-cta--skin-cover elementor-cta elementor-organey-banner"
+      >
+        <div class="elementor-cta__bg-wrapper">
+          <div
+            class="elementor-cta__bg elementor-bg"
+            style="
+              background-image: url(https://demothemedh.b-cdn.net/organey/wp-content/uploads/2021/08/h1_banner-1.jpg);
+            "
+          ></div>
+          <div class="elementor-cta__bg-overlay"></div>
+        </div>
+        <div class="elementor-cta__content">
+          <h2
+            class="
+              elementor-cta__title elementor-cta__content-item
+              elementor-content-item
+            "
+          >
+            Vegan <br />Restaurants
+          </h2>
+
+          <div
+            class="
+              elementor-cta__subtitle elementor-cta__content-item
+              elementor-content-item
+            "
+          >
+            -35% Off
+          </div>
+
+          <div
+            class="
+              elementor-cta__button-wrapper elementor-cta__content-item
+              elementor-content-item
+            "
+          >
+            <a
+              class="elementor-cta__button elementor-button-custom"
+              href="https://demo.leebrosus.com/organey/shop/"
+              target="_blank"
+            >
+              <span>
+                Shop Now
+                <i
+                  aria-hidden="true"
+                  class="organey-icon- organey-icon-short-arrow-left"
+                ></i>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <Banner></Banner>
     <Section1></Section1>
     <Section2></Section2>
+    <Section3></Section3>
     <Section5></Section5>
     <Section6></Section6>
     <Section7></Section7>
@@ -13,6 +14,7 @@
 import Banner from '~/components/home/Banner.vue'
 import Section1 from '~/components/home/Section1.vue'
 import Section2 from '~/components/home/Section2.vue'
+import Section3 from '~/components/home/Section3.vue'
 import Section5 from '~/components/home/Section5.vue'
 import Section6 from '~/components/home/Section6.vue'
 import Section7 from '~/components/home/Section7.vue'
@@ -41,6 +43,7 @@ export default {
     Banner,
     Section1,
     Section2,
+    Section3,
     Section5,
     Section6,
     Section7,
