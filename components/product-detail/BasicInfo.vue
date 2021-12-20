1e7c05629ba3f8239dd17b429a87e787bc1bf3ec
trinhnhatsinh <trinhnhatsinh123@gmail.com>
2021-12-20 14:18:33 +0700

    update

---
+++ components/product-detail/BasicInfo.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="summary entry-summary">
+    <h1 class="product_title entry-title">Large Queen Pineapple</h1>
+    <div class="woocommerce-product-rating">
+      <div class="star-rating" role="img" aria-label="Rated 3.75 out of 5">
+        <span style="width: 75%"
+          >Rated <strong class="rating">3.75</strong> out of 5 based on
+          <span class="rating">4</span> customer ratings</span
+        >
+      </div>
+      <a href="#reviews" class="woocommerce-review-link" rel="nofollow"
+        >(<span class="count">5</span> customer reviews)</a
+      >
+    </div>
+    <p class="price">
+      <del aria-hidden="true"
+        ><span class="woocommerce-Price-amount amount"
+          ><bdi
+            ><span class="woocommerce-Price-currencySymbol">£</span>149.94</bdi
+          ></span
+        ></del
+      >
+      <ins
+        ><span class="woocommerce-Price-amount amount"
+          ><bdi
+            ><span class="woocommerce-Price-currencySymbol">£</span>78.09</bdi
+          ></span
+        ></ins
+      >
+    </p>
+    <div class="woocommerce-product-details__short-description">
+      <ul>
+        <li>Free from the confines of wires and chords</li>
+        <li>Double-ended Coil Cord with 3.5mm Stereo Plugs Included</li>
+        <li>3/4″ Dome Tweeters: 2X and 4″ Woofer: 1X</li>
+      </ul>
+    </div>
+    <p class="stock available-on-backorder">Available on backorder</p>
+    <form
+      class="cart"
+      action="https://demo.leebrosus.com/organey/product/large-queen-pineapple/"
+      method="post"
+      enctype="multipart/form-data"
+    >
+      <div class="quantity buttons_added">
+        <button type="button" class="minus">-</button>
+        <label class="screen-reader-text" for="quantity_61765142bdcce"
+          >Large Queen Pineapple quantity</label
+        >
+        <input
+          type="number"
+          id="quantity_61765142bdcce"
+          class="input-text qty text"
+          step="1"
+          min="1"
+          max=""
+          name="quantity"
+          value="1"
+          title="Qty"
+          size="4"
+          placeholder=""
+          inputmode="numeric"
+        /><button type="button" class="plus">+</button>
+      </div>
+      <button
+        type="submit"
+        name="add-to-cart"
+        value="87"
+        class="single_add_to_cart_button button alt"
+      >
+        Add to cart
+      </button>
+    </form>
+    <button class="woosw-btn woosw-btn-87" data-id="87">Add to wishlist</button>
+    <button class="woosw-btn woosc-btn-87" data-id="87">Compare</button>
+    <div class="organey-single-product-extra">
+      <p>
+        <span
+          style="
+            color: #3d3e3d;
+            font-size: 12px;
+            font-weight: 600;
+            margin-bottom: 15px;
+            display: block;
+          "
+          >Guarantee Safe Checkout</span
+        ><img
+          loading="lazy"
+          style="padding-bottom: 5px"
+          class="alignnone size-medium wp-image-6252 lazyloaded"
+          src="https://demothemedh.b-cdn.net/organey/wp-content/uploads/2021/08/credit-300x17.jpg"
+          alt=""
+          width="300"
+          height="17"
+          sizes="(max-width: 300px) 100vw, 300px"
+          data-ll-status="loaded"
+        /><noscript
+          ><img
+            loading="lazy"
+            style="padding-bottom: 5px"
+            class="alignnone size-medium wp-image-6252"
+            src="https://demothemedh.b-cdn.net/organey/wp-content/uploads/2021/08/credit-300x17.jpg"
+            sizes="(max-width: 300px) 100vw, 300px"
+            alt=""
+            width="300"
+            height="17"
+        /></noscript>
+      </p>
+    </div>
+    <div class="product_meta">
+      <span class="sku_wrapper"
+        >SKU: <span class="sku">synergistic-iron-computer-61818842</span></span
+      >
+      <span class="posted_in"
+        >Categories:
+        <a
+          href="https://demo.leebrosus.com/organey/product-category/fruits/"
+          rel="tag"
+          >Fruits</a
+        >,
+        <a
+          href="https://demo.leebrosus.com/organey/product-category/uncategorized/"
+          rel="tag"
+          >Uncategorized</a
+        ></span
+      >
+    </div>
+    <div class="organey-social-share">
+      <a
+        class="social-facebook"
+        href="https://www.facebook.com/sharer.php?u=https://demo.leebrosus.com/organey/product/large-queen-pineapple/&amp;display=page"
+        target="_blank"
+        title="Share on facebook"
+      >
+        <i class="organey-icon-facebook"></i>
+        <span>Facebook</span>
+      </a>
+      <a
+        class="social-twitter"
+        href="https://twitter.com/home?status= https://demo.leebrosus.com/organey/product/large-queen-pineapple/"
+        target="_blank"
+        title="Share on Twitter"
+      >
+        <i class="organey-icon-twitter"></i>
+        <span>Twitter</span>
+      </a>
+      <a
+        class="social-linkedin"
+        href="https://linkedin.com/shareArticle?mini=true&amp;url=https://demo.leebrosus.com/organey/product/large-queen-pineapple/&amp;title=Large Queen Pineapple"
+        target="_blank"
+        title="Share on LinkedIn"
+      >
+        <i class="organey-icon-linkedin"></i>
+        <span>Linkedin</span>
+      </a>
+      <a
+        class="social-pinterest"
+        href="https://pinterest.com/pin/create/button/?url=https%3A%2F%2Fdemo.leebrosus.com%2Forganey%2Fproduct%2Flarge-queen-pineapple%2F&amp;description=Large+Queen+Pineapple&amp;; ?>"
+        target="_blank"
+        title="Share on Pinterest"
+      >
+        <i class="organey-icon-pinterest-p"></i>
+        <span>Pinterest</span>
+      </a>
+      <a
+        class="social-envelope"
+        href="mailto:?subject=Large Queen Pineapple&amp;body=https://demo.leebrosus.com/organey/product/large-queen-pineapple/"
+        title="Email to a Friend"
+      >
+        <i class="organey-icon-envelope"></i>
+        <span>Email</span>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
---
+++ components/product-detail/InfoTab.vue
@@ -0,0 +1,516 @@
+<template>
+  <div class="info-tab">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link active"
+          id="home-tab"
+          data-bs-toggle="tab"
+          data-bs-target="#home"
+          type="button"
+          role="tab"
+          aria-controls="home"
+          aria-selected="true"
+        >
+          DESCRIPTION
+        </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link"
+          id="profile-tab"
+          data-bs-toggle="tab"
+          data-bs-target="#profile"
+          type="button"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >
+          ADDITIONAL INFORMATION
+        </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button
+          class="nav-link"
+          id="contact-tab"
+          data-bs-toggle="tab"
+          data-bs-target="#contact"
+          type="button"
+          role="tab"
+          aria-controls="contact"
+          aria-selected="false"
+        >
+          REVIEWS(5)
+        </button>
+      </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+      <div
+        class="tab-pane fade"
+        id="home"
+        role="tabpanel"
+        aria-labelledby="home-tab"
+      >
+        <div
+          class="
+            woocommerce-Tabs-panel woocommerce-Tabs-panel--description
+            panel
+            wc-tab
+          "
+          id="tab-description"
+          role="tabpanel"
+          aria-labelledby="tab-title-description"
+          style=""
+        >
+          <div
+            data-elementor-type="wp-post"
+            data-elementor-id="87"
+            class="elementor elementor-87"
+            data-elementor-settings="[]"
+          >
+            <div class="elementor-section-wrap">
+              <section
+                class="
+                  elementor-section
+                  elementor-top-section
+                  elementor-element
+                  elementor-element-c22f2a0
+                  elementor-section-boxed
+                  elementor-section-height-default
+                  elementor-section-height-default
+                "
+                data-id="c22f2a0"
+                data-element_type="section"
+              >
+                <div class="elementor-container elementor-column-gap-default">
+                  <div
+                    class="
+                      elementor-column
+                      elementor-col-100
+                      elementor-top-column
+                      elementor-element
+                      elementor-element-810bff9
+                    "
+                    data-id="810bff9"
+                    data-element_type="column"
+                  >
+                    <div
+                      class="elementor-widget-wrap elementor-element-populated"
+                    >
+                      <div
+                        class="
+                          elementor-element
+                          elementor-element-9d7343b
+                          elementor-widget
+                          elementor-widget-text-editor
+                        "
+                        data-id="9d7343b"
+                        data-element_type="widget"
+                        data-widget_type="text-editor.default"
+                      >
+                        <div class="elementor-widget-container">
+                          <p>
+                            Stock up on the perfect afternoon snack, lunchtime
+                            side or baking choice with a
+                            <b
+                              >Three-Pound Bag of Honeycrisp Apples from Good
+                              &amp; Gather™.</b
+                            >
+                            Boasting the perfect blend of sweet and crisp
+                            flavors, these delicious Honeycrisp apples promise
+                            to hit the spot when you’re craving something fresh
+                            and tasty, and the crisp, juicy texture is sure to
+                            satisfy.
+                          </p>
+                          <p>
+                            Every product that carries the Good &amp; Gather™
+                            name starts with quality ingredients that deliver
+                            great taste, making it easier for you and your
+                            family to eat well, every day. We promise you’ll
+                            love each bite, or your money back.
+                          </p>
+                        </div>
+                      </div>
+                      <div
+                        class="
+                          elementor-element
+                          elementor-element-81798d9
+                          elementor-widget
+                          elementor-widget-text-editor
+                        "
+                        data-id="81798d9"
+                        data-element_type="widget"
+                        data-widget_type="text-editor.default"
+                      >
+                        <div class="elementor-widget-container">
+                          <p class="elementor-widget-container-title">
+                            Item specifics
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section
+                class="
+                  elementor-section
+                  elementor-top-section
+                  elementor-element
+                  elementor-element-2b6e4c0b
+                  elementor-section-boxed
+                  elementor-section-height-default
+                  elementor-section-height-default
+                "
+                data-id="2b6e4c0b"
+                data-element_type="section"
+              >
+                <div class="elementor-container elementor-column-gap-no">
+                  <div
+                    class="
+                      elementor-column
+                      elementor-col-50
+                      elementor-top-column
+                      elementor-element
+                      elementor-element-42ff0b32
+                    "
+                    data-id="42ff0b32"
+                    data-element_type="column"
+                  >
+                    <div
+                      class="elementor-widget-wrap elementor-element-populated"
+                    >
+                      <div
+                        class="
+                          elementor-element
+                          elementor-element-4296f45c
+                          elementor-widget
+                          elementor-widget-text-editor
+                        "
+                        data-id="4296f45c"
+                        data-element_type="widget"
+                        data-widget_type="text-editor.default"
+                      >
+                        <div class="elementor-widget-container">
+                          <ul>
+                            <li><strong>Origin</strong>: Thailand</li>
+                            <li><b>Packing</b>: 400g pack</li>
+                            <li><b>Storage way</b>: room temperature</li>
+                            <li><b>Expiry date</b>: 1 week</li>
+                            <li>
+                              <b>Summary</b>: PP+ iron material, thick legs,
+                              strong bearing capacity, stable and firm,
+                              foldable, free to open and close, no space to
+                              carry, non-slip mat on the bottom, not easy to
+                              slip, easy to carry at home and outdoor, easy to
+                              carry
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="
+                      elementor-column
+                      elementor-col-50
+                      elementor-top-column
+                      elementor-element
+                      elementor-element-7199dad
+                    "
+                    data-id="7199dad"
+                    data-element_type="column"
+                  >
+                    <div
+                      class="elementor-widget-wrap elementor-element-populated"
+                    >
+                      <div
+                        class="
+                          elementor-element
+                          elementor-element-d9f3f36
+                          elementor-widget
+                          elementor-widget-text-editor
+                        "
+                        data-id="d9f3f36"
+                        data-element_type="widget"
+                        data-widget_type="text-editor.default"
+                      >
+                        <div class="elementor-widget-container">
+                          <ul>
+                            <li><b>General Use</b>: Outdoor Furniture</li>
+                            <li><b>Specific Use</b>: Beach Chair</li>
+                            <li><b>Appearance</b>: Modern</li>
+                            <li><b>Size</b>: 23.5*32*5cm</li>
+                            <li><b>Style</b>: FOLDING CHAIR</li>
+                            <li><b>Instructions for use</b>: Eat directly</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="tab-pane fade"
+        id="profile"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
+        <div
+          class="
+            woocommerce-Tabs-panel woocommerce-Tabs-panel--description
+            panel
+            wc-tab
+          "
+          id="tab-description"
+          role="tabpanel"
+          aria-labelledby="tab-title-description"
+          style=""
+        >
+          <table class="woocommerce-product-attributes shop_attributes">
+            <tbody>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--weight
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Weight
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  87 kg
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--dimensions
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Dimensions
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  100 × 137 × 29 cm
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_stand-up
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Stand Up
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>35″L x 24″W x 37-45″H(front to back wheel)</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_folded-wo-wheels
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Folded (w/o wheels)
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>32.5″L x 18.5″W x 16.5″H</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_folded-w-wheels
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Folded (w/ wheels)
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>32.5″L x 24″W x 18.5″H</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_door-pass-through
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Door Pass Through
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>24</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_frame
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Frame
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>Aluminum</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_weight-wo-wheels
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Weight (w/o wheels)
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>20 LBS</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_weight-capacity
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Weight Capacity
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>60 LBS</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div
+        class="tab-pane fade show active"
+        id="contact"
+        role="tabpanel"
+        aria-labelledby="contact-tab"
+      >
+        <div
+          class="
+            woocommerce-Tabs-panel woocommerce-Tabs-panel--description
+            panel
+            wc-tab
+          "
+          id="tab-description"
+          role="tabpanel"
+          aria-labelledby="tab-title-description"
+          style=""
+        >
+          <p>
+            Táo rất giàu chất xơ. Một quả táo cỡ trung bình (100 gram) chứa
+            khoảng 4 gram chất dinh dưỡng này, chiếm 17% giá trị dinh dưỡng cần
+            thiết mỗi ngày. Một phần chất xơ của táo đến từ các chất xơ không
+            hòa tan và chất xơ hòa tan được gọi là pectin.
+          </p>
+          <p>
+            Táo được biết đến là loại trái cây có nhiều vitamin và khoáng chất.
+            Táo cung cấp nhiều vitamin C cho cơ thể. Loại vitamin này là chất
+            chống oxy hóa phổ biến trong trái cây.
+          </p>
+          <table class="woocommerce-product-attributes shop_attributes">
+            <tbody>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--weight
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">Calo</th>
+                <td class="woocommerce-product-attributes-item__value">52</td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--dimensions
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">Nước</th>
+                <td class="woocommerce-product-attributes-item__value">86%</td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_stand-up
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Protein
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>0,3 gram</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_folded-wo-wheels
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Carbs
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>13,8 gram</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_folded-w-wheels
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Đường
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>10,4 gram</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_door-pass-through
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Chất xơ
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>2,4 gram</p>
+                </td>
+              </tr>
+              <tr
+                class="
+                  woocommerce-product-attributes-item
+                  woocommerce-product-attributes-item--attribute_frame
+                "
+              >
+                <th class="woocommerce-product-attributes-item__label">
+                  Chất béo
+                </th>
+                <td class="woocommerce-product-attributes-item__value">
+                  <p>0,2 gram</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
