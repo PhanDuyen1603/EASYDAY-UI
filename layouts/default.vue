877b54ba079765fa62a8d703bf9f3937596ae804
Duyen <huydnjs1905007@fpt.edu.vn>
2021-12-25 17:00:01 +0700

    __ fix css login modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,3 +10,23 @@
     <TheFooter />
   </div>
 </template>
+
+<style lang="scss">
+  .hot-mode.login-form {
+    margin: 0;
+  }
+  @media (min-width: 576px) {
+    #bv-modal-example
+    {
+      .modal-content {
+        border-radius: 30px;
+      }
+      .modal-body {
+        padding: 0 !important;
+      }
+      .modal-dialog {
+          max-width: 454px;
+      }
+    } 
+  }
+</style>
