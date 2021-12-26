6c1c302fa385719a9a39274a806796cb4fe94584
Duyen <huydnjs1905007@fpt.edu.vn>
2021-12-26 22:20:37 +0700

    sticky menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrap__body">
-    <TheHeader />
+    <TheHeader id="desktop_header" />
+    <TheStickyHeader id="mobile_header" />
     <b-modal id="bv-modal-example" hide-footer hide-header>
       <login></login>
     </b-modal>
@@ -10,3 +11,43 @@
     <TheFooter />
   </div>
 </template>
+
+<script>
+  export default {
+    mounted() {
+      // eslint-disable-next-line nuxt/no-env-in-hooks
+      if (process.client) {
+        // eslint-disable-next-line no-undef 
+        const jQuery = $
+        jQuery(window).scroll(function () {
+          if (jQuery(this).scrollTop() > 1) {
+            jQuery('#mobile_header').addClass("sticky-header");
+          } else {
+            jQuery('#mobile_header').removeClass("sticky-header");
+          }
+        });
+      }
+    },
+  }
+
+</script>
+
+<style lang="scss">
+#mobile_header {
+  display: none;
+  margin-top: 0;
+  padding: 1rem 2rem;
+  &.sticky-header {
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 10;
+    background-color: #fff;
+  }
+  .dropdown span, .dropdown2 span, .dropdown3 span {
+    color: #000;
+  }
+}
+</style>
